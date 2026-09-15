f38aaa189ddb5127ae34031124d066a9b40d7719
patimakorn07j <patimakorn.sr.66@ubu.ac.th>
2026-09-15 11:05:48 +0700

    commit

--- StrokeClassification/settings.py
+++ StrokeClassification/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
